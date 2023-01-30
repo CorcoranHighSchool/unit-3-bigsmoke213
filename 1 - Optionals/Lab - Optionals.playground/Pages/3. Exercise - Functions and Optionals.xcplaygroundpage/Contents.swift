911604893b0449f47c5f5c3d36bca0af50fe4ec5
bigsmoke213 <98342378+bigsmoke213@users.noreply.github.com>
2023-01-30 09:22:55 -0800

    3.2

--- 1 - Optionals/Lab - Optionals.playground/Pages/3. Exercise - Functions and Optionals.xcplaygroundpage/Contents.swift
+++ 1 - Optionals/Lab - Optionals.playground/Pages/3. Exercise - Functions and Optionals.xcplaygroundpage/Contents.swift
@@ -5,15 +5,38 @@
  */
 let userInputAge: String = "34e"
 
-
+func checkage(age : String)-> Int?{
+    if let ageInt = Int(age){
+        // this means age is an integer
+        //if they are 18 or older
+        if ageInt >= 18 {
+            print("Welcome!")
+            
+        }else{
+            print("sorry but you are not old enough to use our app.")
+        }
+        return ageInt
+    } else {
+        print("sorry something went wrong please try to enter your age again?")
+        return nil
+    }
+checkage(age: userInputAge)
+checkage(age: "18")
 //:  Go back and update your function to return the age as an integer. Will your function always return a value? Make sure your return type accurately reflects this. Call the function and print the return value.
-
+print(checkage(age: "17"))
+    if let age = checkage(age: "100")
+    {
+        print(age)
+    }
 
 //:  Imagine you are creating an app for making purchases. Write a function that will take in the name of an item for purchase as a `String` and will return the cost of that item as an optional `Double`. In the body of the function, check to see if the item is in stock by accessing it in the dictionary `stock`. If it is, return the price of the item by accessing it in the dictionary `prices`. If the item is out of stock, return `nil`. Call the function and pass in a `String` that exists in the dictionaries below. Print the return value.
 var prices = ["Chips": 2.99, "Donuts": 1.89, "Juice": 3.99, "Apple": 0.50, "Banana": 0.25, "Broccoli": 0.99]
 var stock = ["Chips": 4, "Donuts": 0, "Juice": 12, "Apple": 6, "Banana": 6, "Broccoli": 3]
 
-
+let price = prices["Chips"]
+    func getPrice(of item : String -> Double?){
+        
+    }
 /*:
 [Previous](@previous)  |  page 3 of 6  |  [Next: App Exercise - Food Functions](@next)
  */
--- 2 - Type Casting/Lab - Type Casting.playground/Pages/1. Exercise - Type Casting and Inspection.xcplaygroundpage/Contents.swift
+++ 2 - Type Casting/Lab - Type Casting.playground/Pages/1. Exercise - Type Casting and Inspection.xcplaygroundpage/Contents.swift
@@ -3,19 +3,68 @@
 
  Create a collection of type [Any], including a few doubles, integers, strings, and booleans within the collection. Print the contents of the collection.
  */
- 
+var mystuff : [Any] = [5,20.17,"Cookies", true, true, 708, 78.1, "Parrots","Blanket"]
+print(mystuff)
 
 //:  Loop through the collection. For each integer, print "The integer has a value of ", followed by the integer value. Repeat the steps for doubles, strings and booleans.
- 
+for item in mystuff{
+    //optinoally bimd am iteger calling it numebr
+    if let number = item as? Int{
+        print("the integer has a value of \(number)")
+    }
+    //else optionally bind a double calling it number
+    else if let number = item as? Double{
+        print("the double has a value of \(number)")
+    }
+    //else optionally bind a string calling it word
+    else if let word = item as? String{
+     print("The word is : \(word)")
+    }
+    //else optionally bimd a bool calling it boolean
+    else if let boolean = item as? Bool{
+        print("The boolean is: \(boolean)")
+    }
+}
 
 //:  Create a [String : Any] dictionary, where the values are a mixture of doubles, integers, strings, and booleans. Print the key/value pairs within the collection
- 
+var myLookup : [String: Any] = ["Age": 18, "Name" : "Sammy", "Cost" : 29.95, "True?" : false, "Ask Marc" : "I was trying to figure out why i have these error","Okay": -0, "Are you sure":true]
+print(myLookup)
 
 //:  Create a variable `total` of type `Double` set to 0. Then loop through the dictionary, and add the value of each integer and double to your variable's value. For each string value, add 1 to the total. For each boolean, add 2 to the total if the boolean is `true`, or subtract 3 if it's `false`. Print the value of `total`.
- 
+var total : Double = 0.0
+for value in myLookup.values {
+    
+    //double add the value
+    if let number = value as? Double{
+    total += number
+}
+//int add the value
+    else if let number = value as? Int{
+        total += Double(number)
+    }
+//string add 1
+    else if let _ = value as? String{
+        total += 1
+    }
+//boolean add 2 if true add -3 if false
+    else if let bool = value as? Bool{
+        total += bool ? 2 : -3
+    }
+}
 
 //:  Create a variable `total2` of type `Double` set to 0. Loop through the collection again, adding up all the integers and doubles. For each string that you come across during the loop, attempt to convert the string into a number, and add that value to the total. Ignore booleans. Print the total.
- 
+var total2 : Double = 0
+for value in myLookup.values{
+    // doubles
+    
+    //ints
+    
+    //strings
+    if let string = value as? String{
+        if let number = Double(string){
+            total2 += number 
+    }
+}
 
 /*:
 page 1 of 2  |  [Next: App Exercise - Workout Types](@next)
